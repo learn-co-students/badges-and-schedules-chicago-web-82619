6bc4c55e0b0de04c6bc1138a6ab622698e49d03e
Ashwin Kannan <classifieddatabank@Ashwins-MacBook-Pro.local>
2019-08-29 13:11:56 -0500

    lab complete

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.map {|attendee| badge_maker(attendee)}
+end
+
+def assign_rooms(attendees)
+  assignment = []
+  attendees.each_with_index {|attendee, i|
+    assignment.push("Hello, #{attendee}! You'll be assigned to room #{i+1}!")
+  }
+  assignment
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each {|assignment| puts assignment}
+  assign_rooms(attendees).each {|assignment| puts assignment}
+end
